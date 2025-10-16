19753116341f5dc70e440e1453ed6168ebaafdc3
istszabi <istszabi@gmail.com>
2025-10-16 15:29:47 +0300

    Add files via upload

---
+++ LaborProjekt/src/main/java/com/myproject/laborprojekt/LaborProjekt.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myproject.laborprojekt;
+
+/**
+ *
+ * @author istsz
+ */
+public class LaborProjekt {
+
+    public static void main(String[] args) {
+    }
+}
