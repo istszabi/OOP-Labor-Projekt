02955d45cea161c87735bf9c8bf2578e4409d310
istszabi <istszabi@gmail.com>
2025-10-20 10:31:50 +0300

    Create Playlist.java

---
+++ LaborProjekt/src/main/java/com/myproject/laborprojekt/Playlist.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.laborprojekt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author istsz
+ */
+public class Playlist {
+
+    private String name;
+    private List<Song> songs = new ArrayList<>();
+
+    public Playlist(String name) {
+        this.name = name;
+    }
+    
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+    
+    public void PrintAll() {
+        System.out.println("Playlist:" + name);
+        for (Song s : songs) {
+            System.out.println(" - " + s.getInfo());
+        }
+    }
+    
+    
+}
+
