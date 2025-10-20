48baf16cd7bfab25148dc15b744f8926fbf7c3fc
istszabi <istszabi@gmail.com>
2025-10-20 11:06:01 +0300

    Exercise tipusok, playlist generalas

---
+++ LaborProjekt/src/main/java/com/myproject/laborprojekt/ExerciseType.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.laborprojekt;
+
+/**
+ *
+ * @author istsz
+ */
+public enum ExerciseType {
+    RUNNING,
+    HIIT,
+    CARDIO,
+    YOGA,
+    WEIGHTLIFTING
+}
