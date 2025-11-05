5cc0020330ecddd3d1831c3098336743245258c3
istszabi <istszabi@gmail.com>
2025-11-05 11:46:37 +0200

    Update ApiService.java

--- LaborProjekt/src/main/java/com/myproject/laborprojekt/ApiService.java
+++ LaborProjekt/src/main/java/com/myproject/laborprojekt/ApiService.java
@@ -17,32 +17,34 @@ public class ApiService {
         try {
             // API URL
             String url = API_URL + "?tempo=" + tempo + "&size=" + size + "&seeds=" + String.join(",", seeds);
-            
+
             String body = Unirest.get(url).asString().getBody();
             JsonArray content = JsonParser.parseString(body).getAsJsonObject().getAsJsonArray("content");
 
             for (JsonElement trackElement : content) {
                 JsonObject trackObject = trackElement.getAsJsonObject();
 
-                String title = trackObject.has("trackTitle") ? trackObject.get("trackTitle").getAsString() : "Unknown Title";
-
                 String artist = "Unknown Artist";
                 // ARTIST call
                 if (trackObject.has("artists") && trackObject.getAsJsonArray("artists").size() > 0) {
-                    try { 
-                        artist = trackObject.getAsJsonArray("artists").get(0).getAsJsonObject().get("name").getAsString(); } catch(Exception ignored) {}
+                    try {
+                        artist = trackObject.getAsJsonArray("artists").get(0).getAsJsonObject().get("name").getAsString();
+                    } catch (Exception ignored) {
+                    }
                 }
+                // TITLE call
+                
+                String title = trackObject.has("trackTitle") ? trackObject.get("trackTitle").getAsString() : "Unknown Title";
                 // TEMPO call
                 double songTempo = trackObject.has("tempo") ? trackObject.get("tempo").getAsDouble() : tempo;
-                
+
                 // POPULARITY call
                 int popularity = trackObject.has("popularity") ? trackObject.get("popularity").getAsInt() : 50;
-                
+
                 // DURATION call
-                float duration = trackObject.has("durationMs") ? (float)(trackObject.get("durationMs").getAsDouble() / 60000) : 0;
-                
+                float duration = trackObject.has("durationMs") ? (float) (trackObject.get("durationMs").getAsDouble() / 60000) : 0;
+
                 // Add song to playlist
-                
                 songs.add(new Song(null, title, artist, duration, songTempo, popularity));
             }
 
